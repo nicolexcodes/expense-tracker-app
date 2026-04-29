680274ff429d6e1ae2eeeaaad6c03cd39d938b4a
Katia Rosado <nicole.rosado04@gmail.com>
2026-04-29 14:17:13 -0400

    Add storage.py

---
+++ storage.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+FILE_NAME = "data.json"
+
+def load_expenses():
+  if not os.path.exists(FILE_NAME):
+    return []
+
+  with open(FILE_NAME, "r") as file:
+    return json.load(file)
+
+def save_expenses(expenses):
+  with open(FILE_NAME, "w") as file:
+    json.dump(expenses, file, indent=4)
